a83d1b52c68ade2628701545c9aa83bdcec67121
Florentgbm <96999841+Florentgbm@users.noreply.github.com>
2022-02-13 21:24:19 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,270 @@
-# projet-iai
-API python FLASK permettant de gérer les les livres d'une bibliothèque 
+# Livre API VERSION 1
+l'API nous permettra de gerer les livres d'une bibliothèque.  
+## Commençons
+
+### Installation des Dépendances
+
+#### Python 3.10.1
+#### pip 21.2.4 from C:\Users\hp\AppData\Local\Programs\Python\Python310\lib\site-packages\pip (python 3.10.1)
+
+Suivez les instructions suivantes pour installer l'ancienne version de python sur la plateforme [python docs](https://www.python.org/downloads/windows/#getting-and-installing-the-latest-version-of-python)
+
+#### Dépendances de PIP
+
+Pour installer les dépendances, rentrer dans le dossier projet_iai et ouvrer votre cmd puis exécuter la commande suivante:
+
+```bash ou powershell ou cmd
+pip install -r requirement.txt
+or
+pip3 install -r requirement.txt
+```
+
+Nous passons donc à l'installation de tous les packages se trouvant dans le fichier `requirement.txt`.
+
+##### clé de Dépendances
+
+- [Flask](http://flask.pocoo.org/)  est un petit framework web Python léger, qui fournit des outils et des fonctionnalités utiles qui facilitent la création d’applications web en Python.
+
+- [SQLAlchemy](https://www.sqlalchemy.org/) est un toolkit open source SQL et un mapping objet-relationnel écrit en Python et publié sous licence MIT. SQLAlchemy a opté pour l'utilisation du pattern Data Mapper plutôt que l'active record utilisés par de nombreux autres ORM
+
+- [Flask-CORS](https://flask-cors.readthedocs.io/en/latest/#) is the extension we'll use to handle cross origin requests from our frontend server. 
+
+## Démarrer le serveur
+
+Pour démarrer le serveur sur Linux ou Mac, executez:
+
+```bash
+export FLASK_APP=app.py
+export FLASK_ENV=development
+flask run
+```
+Pour le démarrer sur Windows, executez:
+
+```bash
+set FLASK_APP=app.py
+set FLASK_ENV=development
+flask run
+``` 
+
+## API REFERENCE
+
+Getting starter
+
+Base URL: At present this app can only be run locally and is not hosted as a base URL. The backend app is hosted at the default, http://localhost:5000; which is set as a proxy in frontend configuration.
+
+## Type d'erreur
+Les erreurs sont renvoyées sous forme d'objet au format Json:
+{
+    "success":False
+    "error": 404
+    "message":"Ressource non disponible"
+}
+
+L'API vous renvoie 4 types d'erreur:
+. 400: Bad request ou ressource non disponible
+. 500: Internal server error
+. 404: Not found
+
+## Endpoints
+. ## GET/Livres
+
+    GENERAL:
+        Cet endpoint retourne la liste des objets livres 
+    
+        
+    EXEMPLE: curl http://localhost:5000/Livres
+```
+        {
+    "Livres":[
+    {
+        "AUTEUR ": "Ferdinand Oyono",
+        "Date de la publication": "Mon, 25 Dec 2000 00:00:00 GMT",
+        "EDITEUR ": "gbemou",
+        "ID": 2,
+        "ISBN": "10SS",
+        "TITRE": "Le vieux nègre et la médaille",
+        "categorie": 1
+    },
+    {
+        "AUTEUR ": "gbemou",
+        "Date de la publication": "Sun, 10 Dec 2006 00:00:00 GMT",
+        "EDITEUR ": "florent",
+        "ID": 7,
+        "ISBN": "14gb",
+        "TITRE": "Sous l'orage",
+        "categorie": 2
+    },
+    {
+        "AUTEUR ": "Ferdinand oyono",
+        "Date de la publication": "Sun, 10 Dec 2006 00:00:00 GMT",
+        "EDITEUR ": "GBEMOU",
+        "ID": 10,
+        "ISBN": "13gb",
+        "TITRE": "le vieux nègre et la medaille",
+        "categorie": 2
+    }
+]
+```
+
+.##GET/Livres(id)
+  GENERAL:
+  Cet endpoint permet de récupérer les informations d'un livre particulier s'il existe par le biais de l'ID.
+
+    EXEMPLE: http://localhost:5000/books/2
+```
+ {
+    "AUTEUR ": "Ferdinand Oyono",
+    "Date de la publication": "Mon, 25 Dec 2000 00:00:00 GMT",
+    "EDITEUR ": "gbemou",
+    "ID": 2,
+    "ISBN": "10SS",
+    "TITRE": "Le vieux nègre et la médaille",
+    "categorie": 1
+}
+```
+
+
+. ## DELETE/Livres(id)
+
+    GENERAL:
+        Supprimer un livre si l'ID existe. Retourne l'ID du livre supprimé, la valeur du succès et le nombre de livres qui restent .
+
+        EXEMPLE: curl -X DELETE http://localhost:5000/Livres/10
+{
+    "id_Livre": 10,
+    "new_total": 2,
+    "success": true
+}
+```
+
+. ##PATCH/Livres(id)
+  GENERAL:
+  Cet endpoint permet de mettre à jour, le titre, l'auteur, et l'éditeur du livre.
+  Il retourne un livre mis à jour.
+
+  EXEMPLE.....Avec Patch
+  ``` curl -X PATCH http://localhost:5000/books/1 -H "Content-Type:application/json" -d '{
+  "titre":"une vie de boy",
+  "auteur":"Ferdinand Oyono",
+  "editeur":"Florent"
+}
+  ```
+  ```
+ {
+    "Livre": {
+        "AUTEUR ": "Ferdinand Oyono",
+        "Date de la publication": "Mon, 25 Dec 2000 00:00:00 GMT",
+        "EDITEUR ": "Florent",
+        "ID": 2,
+        "ISBN": "10SS",
+        "TITRE": "une vie de boy",
+        "categorie": 1
+    },
+    "success": true
+}
+    ```
+
+. ## GET/categories
+
+    GENERAL:
+        Cet endpoint retourne la liste des categories de livres. 
+    
+        
+    EXEMPLE: curl http://localhost:5000/categories
+
+[
+    {
+        "ID": 1,
+        "Libelle categories :": "roman"
+    },
+    {
+        "ID": 4,
+        "Libelle categories :": "roman"
+    },
+    {
+        "ID": 5,
+        "Libelle categories :": "roman"
+    },
+    {
+        "ID": 6,
+        "Libelle categories :": "press"
+    },
+    {
+        "ID": 7,
+        "Libelle categories :": "loisirs"
+    },
+    {
+        "ID": 2,
+        "Libelle categories :": "bande dessinee"
+    }
+]
+```
+
+.##GET/categories(id)
+  GENERAL:
+  Cet endpoint permet de récupérer les informations d'une categorie si elle existe par le biais de l'ID.
+
+    EXEMPLE: http://localhost:5000/categories/2
+```
+{
+    "ID": 2,
+    "Libelle categories :": "bande dessinee"
+}
+```
+
+. ## DELETE/categories (categories_id)
+
+    GENERAL:
+        Supprimer une categorie si l'ID existe. Retourne l'ID da la catégorie supprimé, la valeur du succès et le nouveau total.
+        NB: si il exite un livre qui appartient à la categorie qu'on veut supprimer on aura une erreur 404 car il ne faudrait pas 
+        oublier la contrainte de clé primaire.On ne peut pas avoir un livre qui existe dans une categorie inexistante.   
+
+        EXEMPLE: curl -X DELETE http://localhost:5000/categories/5
+```
+{
+    "id_cat": 5,
+    "status": 200,
+    "success": true,
+    "total_categories": 5
+}
+```
+
+. ##PATCH/categories(id)
+  GENERAL:
+  Cet endpoint permet de mettre à jour le libelle ou le nom de la categorie.
+  Il retourne la categorie avec une nouvelle nom.
+
+  EXEMPLE.....Avec Patch
+  ``` curl -X PATCH 'http://localhost:5000/categories/4' -H "Content-Type:application/json" -d '{
+    "libelle_categorie":"Livre de programmation"
+}
+  ```
+  ```
+{
+    "categorie": {
+        "ID": 2,
+        "Libelle categories :": "Livre de programmation"
+    },
+    "success": true
+}
+
+.##GET/categories(id)/Livres
+  GENERAL:
+  Cet endpoint permet de lister les livres appartenant à une categorie donnée.
+  Il renvoie la classe de la categorie et les livres l'appartenant.
+
+    EXEMPLE: http://localhost:5000/categories/2/Livres
+```
+[
+    {
+        "AUTEUR ": "gbemou",
+        "Date de la publication": "Sun, 10 Dec 2006 00:00:00 GMT",
+        "EDITEUR ": "florent",
+        "ID": 7,
+        "ISBN": "14gb",
+        "TITRE": "Sous l'orage",
+        "categorie": 2
+    }
+]
+```
+
---
+++ projet_iai.py
@@ -0,0 +1,222 @@
+
+import os
+from flask_migrate import Migrate
+from flask import Flask, jsonify, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus
+from dotenv import load_dotenv
+def create_app(test_config=None):
+    projet_iai = Flask(__name__, instance_relative_config=True)
+load_dotenv()
+projet_iai=Flask(__name__)
+
+motdepasse=quote_plus(os.getenv('password'))
+hostname=os.getenv('host')
+
+projet_iai.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:{}@{}:5432/projet_python'.format(motdepasse,hostname)
+projet_iai.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(projet_iai)
+migrate=Migrate(projet_iai,db)
+@projet_iai.after_request
+def after_request(response):
+ response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+ response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTION')
+ return response
+class Categorie(db.Model):
+    __tablename__='CATEGORIES'
+    id=db.Column(db.Integer,primary_key=True)
+    libelle_categorie=db.Column(db.String(50),nullable=False)
+    Livre=db.relationship("Livre",backref="Categorie",lazy=True)
+    def __init__(self,libelle_categorie):
+        self.libelle_categorie=libelle_categorie
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def format(self):
+        return{
+        'ID': self.id,
+        'Libelle categories :': self.libelle_categorie
+        }
+class Livre(db.Model):
+    __tablename__='LIVRES'
+    id=db.Column(db.Integer,primary_key=True,unique=True)
+    isbn=db.Column(db.String(30),nullable=False,unique=True)
+    titre=db.Column(db.String(40),nullable=False)
+    date_publication=db.Column(db.DateTime,nullable=False)
+    auteur=db.Column(db.String(50),nullable=False)
+    editeur=db.Column(db.String(50),nullable=False)
+    categorie_id=db.Column(db.Integer,db.ForeignKey(Categorie.id),nullable=False)
+    def __init__(self,isbn,titre,date_publication,auteur,editeur,categorie_id):
+        self.isbn=isbn,
+        self.titre=titre,
+        self.date_publication=date_publication,
+        self.auteur=auteur,
+        self.editeur=editeur,
+        self.categorie_id=categorie_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def format(self):
+        return{
+        'ID': self.id,
+        'ISBN': self.isbn,
+        'TITRE': self.titre,
+        'Date de la publication' :self.date_publication,
+        'AUTEUR ' : self.auteur,
+        'EDITEUR ': self.editeur,
+        'categorie': self.categorie_id
+        }
+db.create_all()
+
+@projet_iai.route('/')
+def BG():
+    return 'ANANI azui'
+#################################################  
+        # la liste des livres
+#################################################
+@projet_iai.route('/Livres',methods=['GET'])
+def liste_livre():
+        try:
+            livre = Livre.query.all()
+            livre=[l.format() for l in livre]
+            return jsonify(livre)
+        except:
+            abort(404)
+        finally:
+            db.session.close()
+#################################################  
+        # Chercher un livre  par son id
+#################################################
+@projet_iai.route('/Livres/<int:id>',methods=['GET'])
+def rechercher_livre_id(id):
+    livre = Livre.query.get(id)
+    if livre is None:
+        abort(404)
+    else:
+        return livre.format()
+           
+#####################################
+  #la liste des categories
+#####################################
+@projet_iai.route('/categories',methods=['GET'])
+def liste_categories():
+        try:
+            categorie = Categorie.query.all()
+            categorie=[l.format() for l in categorie]
+            return jsonify(categorie)
+        except:
+            abort(404)
+        finally:
+            db.session.close()
+################################################
+  # Lister la liste des livres d'une categorie
+################################################
+@projet_iai.route('/categories/<int:id>/Livres',methods=['GET'])
+def listes_livres_categorie(id):
+    try:
+        liste2=Livre.query.filter_by(categorie_id=id).all()
+        liste2=[l.format() for l in liste2]
+        return jsonify(liste2)
+    except:
+       abort(404)
+    finally:
+        db.session.close()
+###############################################
+   #chercher une categorie par son id
+###############################################
+@projet_iai.route('/categories/<int:id>',methods=['GET'])
+def rechercher_categorie_id(id):
+    categorie = Categorie.query.get(id)
+    if categorie is None:
+        abort(404)
+    else:
+        return categorie.format()
+############################
+    # Supprimer un livre
+############################
+@projet_iai.route('/Livres/<int:id>', methods=['DELETE'])
+def del_livre(id):
+        try:
+            livre = Livre.query.get(id)
+            livre.delete()
+            return jsonify({
+                'success': True,
+                'id_Livre': id,
+                'new_total': livre.query.count()
+            })
+        except:
+            abort(404)
+        finally:
+            db.session.close()
+
+#############################
+    # Supprimer une categorie
+#############################
+
+@projet_iai.route('/categories/<int:id>', methods=['DELETE'])
+def del_categories(id):
+        try:
+            categorie = Categorie.query.get(id)
+            categorie.delete()
+            return jsonify({
+                'success': True,
+                'status': 200,
+                'id_cat': id,
+                'total_categories': Categorie.query.count()
+            })
+        except:
+            abort(404)
+        finally:
+            db.session.close()
+
+###########################################
+    # Modifier les informations d'un livre
+###########################################
+
+@projet_iai.route('/Livres/<int:id>', methods=['PATCH'])
+def modifier_livre(id):
+        body = request.get_json()
+        livre = Livre.query.get(id)
+        try:
+            if 'titre' in body and 'auteur' in body and 'editeur' in body:
+                livre.titre = body['titre']
+                livre.auteur = body['auteur']
+                livre.editeur = body['editeur']
+                livre.update()
+                return jsonify({
+                'success':True,
+                 'Livre' :livre.format()
+                 })
+        except:
+            abort(404)
+        finally:
+           db.session.close()
+########################################
+ # Modifier le libellé d'une categorie
+########################################
+
+@projet_iai.route('/categories/<int:id>', methods=['PATCH'])
+def change_name(id):
+        body = request.get_json()
+        cate = Categorie.query.get(id)
+        try:
+            if 'libelle_categorie' in body:
+                cate.libelle_categorie = body['libelle_categorie']
+                cate.update()
+                return jsonify({
+                'success':True,
+                 'categorie': cate.format()
+                })
+        except:
+            abort(404)
